d2e89065d6bfd9b6b356eea2d2ceec108648ce63
Alex Kholodniak <alexandrkholodniak@gmail.com>
2024-06-09 08:33:18 -0500

    Improve Rasch Model implementation with learning rate and better documentation (#7)

--- spec/irt_ruby/rasch_model_spec.rb
+++ spec/irt_ruby/rasch_model_spec.rb
@@ -4,26 +4,31 @@ require "spec_helper"
 
 RSpec.describe IrtRuby::RaschModel do
   let(:data) { Matrix[[1, 0, 1], [0, 1, 0], [1, 1, 1]] }
-  let(:irt_model) { IrtRuby::RaschModel.new(data, max_iter: 2000) }
+  let(:model) { IrtRuby::RaschModel.new(data, max_iter: 2000) }
+
+  describe "#initialize" do
+    it "initializes with data" do
+      expect(model.instance_variable_get(:@data)).to eq(data)
+    end
+  end
 
   describe "#sigmoid" do
-    it "calculates the sigmoid of a value" do
-      expect(irt_model.sigmoid(0)).to be_within(0.01).of(0.5)
-      expect(irt_model.sigmoid(2)).to be_within(0.01).of(0.88)
+    it "calculates the sigmoid function" do
+      expect(model.sigmoid(0)).to eq(0.5)
     end
   end
 
   describe "#likelihood" do
     it "calculates the likelihood of the data" do
-      expect(irt_model.likelihood).to be_a(Float)
+      expect(model.likelihood).to be_a(Float)
     end
   end
 
   describe "#fit" do
     it "fits the model and returns abilities and difficulties" do
-      results = irt_model.fit
-      expect(results[:abilities].size).to eq(3)
-      expect(results[:difficulties].size).to eq(3)
+      result = model.fit
+      expect(result[:abilities].size).to eq(data.row_count)
+      expect(result[:difficulties].size).to eq(data.column_count)
     end
   end
 end
