f83cff675b0ae398c9260678b1307a0f60f068ae
Alex Kholodniak <alexandrkholodniak@gmail.com>
2026-06-05 20:01:53 -0500

    fix: validate model optimization options (#18)

--- lib/irt_ruby.rb
+++ lib/irt_ruby.rb
@@ -2,6 +2,7 @@
 
 require "irt_ruby/version"
 require "matrix"
+require "irt_ruby/model_options_validator"
 require "irt_ruby/response_data_validator"
 require "irt_ruby/rasch_model"
 require "irt_ruby/two_parameter_model"
